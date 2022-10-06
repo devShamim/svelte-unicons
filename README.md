6280e4715128e7a802e0ced5263f2ad6490402b1
devShamim <shamimeforb@gmail.com>
2022-10-06 22:11:00 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# svelte-unicons
+
+## Install
+
+```bash
+yarn add --dev svelte-unicons
+```
+
+or use NPM
+
+```bash
+npm install --save-dev svelte-unicons
+```
+
+## Usage
+
+```html
+<script>
+  // Only import what you need!
+  import { UilEye, UilArrowRight, ... } from 'svelte-unicons'
+</script>
+
+<UilEye size="24" />
+<UilArrowRight size="1.5x" />
+```
+
+See all icons and usage here: https://iconscout.com/unicons/explore/line
+
+## Author
+
+This package is completely based on @iconscout/unicons
---
+++ build.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const unicons = require('@iconscout/unicons/json/line.json')
+const {
+  pascalCase
+} = require('pascal-case')
+const fs = require('fs-extra')
+const cheerio = require('cheerio')
+
+const handleComponentName = name => name.replace(/\-(\d+)/, '$1')
+
+const icons = unicons.map(item => ({
+  name: item.name,
+  pascalCasedComponentName: 'Uil' + pascalCase(`${handleComponentName(item.name)}`),
+  kebabCasedComponentName: `Uil-${handleComponentName(item.name)}`,
+  iconSvg: item.svg
+}))
+
+Promise.all(icons.map(icon => {
+  const svgFile = fs.readFileSync(path.resolve(`node_modules/@iconscout/unicons/${icon.iconSvg}`), 'utf-8')
+  let data = svgFile.replace(/<svg[^>]+>/gi, '').replace(/<\/svg>/gi, '')
+  const $ = cheerio.load(data, {
+    xmlMode: true
+  })
+  const svgPath = $('path').attr('d')
+
+  let component = `
+    <script>
+      export let size = "24";
+      export let strokeWidth = 2;
+      let customClass = "";
+      export { customClass as class };
+
+      if (size !== "100%") {
+        size = size.slice(-1) === 'x'
+              ? size.slice(0, size.length -1) + 'em'
+              : parseInt(size) + 'px';
+      }
+    </script>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="{strokeWidth}" stroke-linecap="round" stroke-linejoin="round" class="unicons ${icon.name} {customClass}">
+      <path
+      d="${svgPath}"
+      />
+    </svg>
+  `
+
+  const filepath = `./src/icons/${icon.pascalCasedComponentName}.svelte`
+  return fs.ensureDir(path.dirname(filepath))
+    .then(() => fs.writeFile(filepath, component, 'utf8'))
+})).then(async () => {
+  const main = icons
+    .map(icon => `export { default as ${icon.pascalCasedComponentName} } from './icons/${icon.pascalCasedComponentName}.svelte'`)
+    .join('\n\n')
+  const types = '/// <reference types="svelte" />\nimport {SvelteComponentTyped} from "svelte/internal"\n' +
+    icons.map(icon => `export class ${icon.pascalCasedComponentName} extends SvelteComponentTyped<{size?: string, strokeWidth?: number, class?: string}> {}`).join("\n")
+  await fs.outputFile("index.d.ts", types, 'utf8');
+  return await fs.outputFile('./src/index.js', main, 'utf8')
+})
